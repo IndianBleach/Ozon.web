a4e5df5799ad463480240ec2ce21994f423c9fc3
IndianBleach <alabamableach@gmail.com>
2023-08-13 21:34:22 +0300

    Init main mvc project

---
+++ src/Ozon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ozon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
