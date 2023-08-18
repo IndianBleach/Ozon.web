828a1351d07e53c68f4636077cda04f85223f8ef
IndianBleach <alabamableach@gmail.com>
2023-08-18 16:19:44 +0300

    change global project structure

---
+++ src/Services/Services.Authorization/Authorization.Api/Controllers/AuthorizeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorizeController
+    {
+        // signin -> {access_token, refresh_token}
+        // signout -> {ok, bad}
+        // signup -> {access_token, refresh_token}
+
+        // db (mssql)
+        
+        // docker bridge -> services.accounts (findByUserName)
+        // (описать внешний сервис для предоставления сервису авторизации)
+    }
+}
