80aac9826506227ccdabb9fcc6b53810dc97024d
IndianBleach <alabamableach@gmail.com>
2023-08-20 14:56:04 +0300

    grpc: add new calls

--- src/Services/Services.Accounts/Accounts.Grpc/GrpcServices/UserAccountGrpcService.cs
+++ src/Services/Services.Accounts/Accounts.Grpc/GrpcServices/UserAccountGrpcService.cs
@@ -1,7 +1,11 @@
-﻿using Accounts.Infrastructure.Specifications;
+﻿using Accounts.Data.Entities.AccountEntities;
+using Accounts.Data.Entities.RoleEntities;
+using Accounts.Infrastructure.Specifications;
+using Accounts.Infrastructure.Specifications.Roles;
 using Common.DataQueries;
 using Common.Repositories;
 using Common.Specifications;
+using Data.Entities;
 using Grpc.Accounts;
 using Grpc.Accounts.Common;
 using Grpc.Core;
@@ -12,14 +16,112 @@ namespace Accounts.Grpc.GrpcServices
     {
         private readonly IServiceRepository<UserAccount> _accountRepository;
 
-        public UserAccountGrpc(IServiceRepository<UserAccount> accountRepository)
+        private readonly IServiceRepository<UserRole> _roleRepository;
+
+        private readonly IServiceRepository<ApplicationUser> _userRepository;
+
+        public UserAccountGrpc(
+            IServiceRepository<ApplicationUser> userRepository,
+            IServiceRepository<UserRole> roleRepository,
+            IServiceRepository<UserAccount> accountRepository)
         {
+            _userRepository = userRepository;
             _accountRepository = accountRepository;
+            _roleRepository = roleRepository;
+        }
+
+        public override async Task<CreateRoleRespone> CreateRole(CreateRoleRequest request, ServerCallContext context)
+        {
+            UserRole role = new UserRole(
+                name: request.RoleName);
+
+            QueryResult<string> query = _roleRepository.Create(role);
+
+            if (!query.IsSuccessed)
+                return new CreateRoleRespone
+                {
+                    QueryState = new QueryResultState
+                    {
+                        IsSuccessed = false,
+                        ErrorMessage = query.StatusMessage
+                    }
+                };
+
+            return new CreateRoleRespone
+            {
+                RoleId = query.Value
+            };
+        }
+
+        public override async Task<CreateCreateUserResponse> CreateUser(CreateUserRequest request, ServerCallContext context)
+        {
+            ApplicationUser user = new ApplicationUser(
+                firstName: request.FirstName,
+                lastName: request.SecondName,
+                email: request.Email,
+                dateCreated: DateTime.Now);
+
+            QueryResult<string> query = _userRepository.Create(user);
+
+            if (!query.IsSuccessed)
+                return new CreateCreateUserResponse
+                {
+                    QueryState = new QueryResultState
+                    {
+                        ErrorMessage = query.StatusMessage,
+                        IsSuccessed = false
+                    }
+                };
+
+            return new CreateCreateUserResponse
+            {
+                UserId = query.Value
+            };
+        }
+
+        public override async Task<CreateClientUserAccountResponse> CreateClientAccount(CreateClientUserAccountRequest request, ServerCallContext context)
+        {
+            ISpecification<UserRole> spec = new RoleByNameSpec("client");
+
+            UserRole? getRole = _roleRepository.FirstOrDefault(spec);
+
+            if (getRole == null)
+                return new CreateClientUserAccountResponse
+                {
+                    QueryState = new QueryResultState
+                    {
+                        ErrorMessage = "Role (*client) not found",
+                        IsSuccessed = false
+                    }
+                };
+
+            UserAccount account = new UserAccount(
+                userId: request.UserId,
+                passwordHash: request.UserPassword,
+                userName: request.UserName,
+                getRole.Id);
+
+            QueryResult<string> createQuery = _accountRepository.Create(account);
+
+            if (!createQuery.IsSuccessed)
+                return new CreateClientUserAccountResponse
+                {
+                    QueryState = new QueryResultState
+                    {
+                        ErrorMessage = createQuery.StatusMessage,
+                        IsSuccessed = false
+                    }
+                };
+
+            return new CreateClientUserAccountResponse
+            {
+                UserAccountId = createQuery.Value
+            };
         }
 
         public override async Task<FindUserAccountByUserNameResponse> FindAccountByUserName(FindUserAccountByUserNameRequest request, ServerCallContext context)
         {
-            ISpecification<UserAccount> spec = (ISpecification<UserAccount>)new AccountByNameSpec(request.UserName);
+            ISpecification<UserAccount> spec = new AccountByNameSpec(request.UserName);
 
             UserAccount? result = _accountRepository.FirstOrDefault(spec);
                 
@@ -35,14 +137,14 @@ namespace Accounts.Grpc.GrpcServices
 
             return new FindUserAccountByUserNameResponse
             {
-                UserAccount = new UserAccount
+                UserAccount = new UserAccountRead
                 {
-                    AccountId = result.AccountId,
+                    AccountId = result.Id,
                     Role = result.Role != null ?
-                        new AccountRole
+                        new AccountRoleRead
                         {
-                            RoleId = result.Role.RoleId,
-                            RoleNormalizeName = result.Role.RoleNormalizeName
+                            RoleId = result.Role.Id,
+                            RoleNormalizeName = result.Role.Name
                         }
                         : null,
                     UserId = result.UserId,
@@ -53,7 +155,7 @@ namespace Accounts.Grpc.GrpcServices
 
         public override async Task<FindUserAccountByIdResponse> FindAccountById(FindUserAccountByIdRequest request, ServerCallContext context)
         {
-            ISpecification<UserAccount> spec = (ISpecification<UserAccount>)new AccountByIdSpec(request.UserAccountId);
+            ISpecification<UserAccount> spec = new AccountByIdSpec(request.UserAccountId);
 
             var result = _accountRepository.FirstOrDefault(spec);
 
@@ -69,14 +171,14 @@ namespace Accounts.Grpc.GrpcServices
 
             return new FindUserAccountByIdResponse
             {
-                UserAccount = new UserAccount
+                UserAccount = new UserAccountRead
                 {
-                    AccountId = result.AccountId,
+                    AccountId = result.Id,
                     Role = result.Role != null ?
-                        new AccountRole
+                        new AccountRoleRead
                         {
-                            RoleId = result.Role.RoleId,
-                            RoleNormalizeName = result.Role.RoleNormalizeName
+                            RoleId = result.Role.Id,
+                            RoleNormalizeName = result.Role.Name
                         }
                         : null,
                     UserId = result.UserId,
---
+++ src/Services/Services.Accounts/Accounts.Infrastructure/Specifications/Roles/RoleByIdSpec.cs
@@ -0,0 +1,17 @@
+﻿using Accounts.Data.Entities.RoleEntities;
+using Common.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Infrastructure.Specifications.Roles
+{
+    public class RoleByIdSpec : BaseSpecification<UserRole>
+    {
+        public RoleByIdSpec(string roleId)
+            : base(x => x.Id.Equals(roleId))
+        {}
+    }
+}
