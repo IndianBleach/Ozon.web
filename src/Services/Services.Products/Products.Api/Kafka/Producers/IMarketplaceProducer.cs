cad83635cc74507529aba0160ce568d1ab99f754
ghot <84047808+IndianBleach@users.noreply.github.com>
2023-09-21 13:26:27 +0300

    productRegistry: add messageBus consumers

---
+++ src/Services/Services.Products/Products.Api/Kafka/Producers/IMarketplaceProducer.cs
@@ -0,0 +1,11 @@
+﻿using Common.DTOs.ApiRequests.Products;
+
+namespace Products.Api.Kafka.Producers
+{
+    public interface IMarketplaceProducer
+    {
+        void AddMarketplaceSeller(
+            ProductSellerApiPost seller,
+            string sellerId);
+    }
+}
