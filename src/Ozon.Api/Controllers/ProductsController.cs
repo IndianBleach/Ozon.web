5995585f6a813b4f57e7521c515e0de58e0be17a
ghot <84047808+IndianBleach@users.noreply.github.com>
2023-09-21 13:17:16 +0300

    Api: add gateway

--- src/Ozon.Api/Controllers/ProductsController.cs
+++ src/Ozon.Api/Controllers/ProductsController.cs
@@ -15,58 +15,58 @@ namespace Ozon.Api.Controllers
             Console.WriteLine("[CTOR (products)] " + DateTime.Now);
         }
 
-        [HttpPost("/[controller]/sellers")]
-        public async Task<IActionResult> CreateProductSeller(
-            string bank_number,
-            string contact_mail,
-            string contact_site,
-            string desc,
-            string title)
-        {
-            var productsClient = new ProductServiceGrpc.ProductServiceGrpcClient(_productsChannel);
+        //[HttpPost("/[controller]/sellers")]
+        //public async Task<IActionResult> CreateProductSeller(
+        //    string bank_number,
+        //    string contact_mail,
+        //    string contact_site,
+        //    string desc,
+        //    string title)
+        //{
+        //    var productsClient = new ProductServiceGrpc.ProductServiceGrpcClient(_productsChannel);
 
-            var res = productsClient.CreateProductSeller(new CreateProductSellerRequest
-            { 
-                BankAccountNumber = bank_number,
-                ContactEmail = contact_mail,
-                ContactSite = contact_site,
-                Description = desc,
-                SpecialCode = bank_number,
-                Title = title
-            });
+        //    var res = productsClient.CreateProductSeller(new CreateProductSellerRequest
+        //    { 
+        //        BankAccountNumber = bank_number,
+        //        ContactEmail = contact_mail,
+        //        ContactSite = contact_site,
+        //        Description = desc,
+        //        SpecialCode = bank_number,
+        //        Title = title
+        //    });
 
-            return Ok(res);
-        }
+        //    return Ok(res);
+        //}
 
-        [HttpPost("/[controller]/")]
-        public async Task<IActionResult> CreateProduct(
-            string seller_id,
-            string desc,
-            double price,
-            string title)
-        {
-            var productsClient = new ProductServiceGrpc.ProductServiceGrpcClient(_productsChannel);
+        //[HttpPost("/[controller]/")]
+        //public async Task<IActionResult> CreateProduct(
+        //    string seller_id,
+        //    string desc,
+        //    double price,
+        //    string title)
+        //{
+        //    var productsClient = new ProductServiceGrpc.ProductServiceGrpcClient(_productsChannel);
 
-            var res = productsClient.CreateProductAsync(new CreateProductRequest
-            { 
-                DefaultPrice = price,
-                SellerId = seller_id,
-                DefaultDescription = desc,
-                DefaultTitle = title
-            });
+        //    var res = productsClient.CreateProductAsync(new CreateProductRequest
+        //    { 
+        //        DefaultPrice = price,
+        //        SellerId = seller_id,
+        //        DefaultDescription = desc,
+        //        DefaultTitle = title
+        //    });
 
-            return Ok(res);
-        }
+        //    return Ok(res);
+        //}
 
 
-        [HttpGet("/[controller]/all")]
-        public async Task<IActionResult> GetAllProducts()
-        {
-            var productsClient = new ProductServiceGrpc.ProductServiceGrpcClient(_productsChannel);
+        //[HttpGet("/[controller]/all")]
+        //public async Task<IActionResult> GetAllProducts()
+        //{
+        //    var productsClient = new ProductServiceGrpc.ProductServiceGrpcClient(_productsChannel);
 
-            var res = productsClient.GetAllProducts(new GetAllProductsRequest());
+        //    var res = productsClient.GetAllProducts(new GetAllProductsRequest());
 
-            return Ok(res);
-        }
+        //    return Ok(res);
+        //}
     }
 }
