6d1b367483481c4255493a70b2092e43b60291b7
ghot <84047808+IndianBleach@users.noreply.github.com>
2023-09-21 13:27:57 +0300

    productStorage: change controllers with api gatewat specific

---
+++ src/Services/Services.Storage/Storage.Api/Controllers/DeveloperController.cs
@@ -0,0 +1,24 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Storage.Api.Kafka.Services;
+
+namespace Storage.Api.Controllers
+{
+    public class DeveloperController : ControllerBase
+    {
+        private readonly IBackgroundJobClient _jobClient;
+
+        public DeveloperController(IBackgroundJobClient jobClient)
+        {
+            _jobClient = jobClient;
+        }
+
+        [HttpPost("/consumers/")]
+        public async Task<IActionResult> StartConsumers()
+        {
+            var id = _jobClient.Enqueue<CService_ProductStorageRegistration>((service) => service.ConsumeAsync());
+
+            return Ok(id);
+        }
+    }
+}
---
+++ src/Services/Services.Storage/Storage.Api/Controllers/StoragesController.cs
@@ -0,0 +1,189 @@
+using AutoMapper;
+using Azure.Core;
+using Common.DataQueries;
+using Common.DTOs.Storage;
+using Common.Grpc.Extensions;
+using Common.Repositories;
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Storage.Api.Kafka.Producers;
+using Storage.Api.Kafka.Services;
+using Storage.Data.Entities.Actions;
+using Storage.Data.Entities.Address;
+using Storage.Data.Entities.Employees;
+using Storage.Data.Entities.Products;
+using Storage.Data.Entities.Storage;
+using Storage.Infrastructure.Mappers;
+using Storage.Infrastructure.Specifications;
+using System.Threading;
+
+namespace Storage.Api.Controllers
+{
+    //[ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class StoragesController : ControllerBase
+    {
+        private IMarketplaceProducer _marketplaceProducer;
+
+        private IMapper _mapper;
+
+        private readonly ILogger<StoragesController> _logger;
+
+        private readonly IServiceRepository<MarketStorage> _storageRepository;
+
+        private readonly IServiceRepository<StorageCell> _storageCellRepository;
+
+        private readonly IServiceRepository<AddressPoint> _addressRepository;
+
+        private readonly IServiceRepository<StorageEmployee> _employeeRepository;
+
+        private readonly IServiceRepository<StorageActionType> _actionTypesRepository;
+
+        private IServiceRepository<StorageProduct> _productsRepository;
+
+        public StoragesController(
+            IMarketplaceProducer marketplaceProducer,
+            ILogger<StoragesController> logger,
+            IServiceRepository<MarketStorage> storageRepository,
+            IServiceRepository<StorageCell> cellRepository,
+            IServiceRepository<AddressPoint> addrRepository,
+            IServiceRepository<StorageEmployee> employeeRepository,
+            IServiceRepository<StorageActionType> actionTypesRepository,
+            IServiceRepository<StorageProduct> productsRepository)
+        {
+            _marketplaceProducer = marketplaceProducer;
+            _productsRepository = productsRepository;
+            _storageCellRepository = cellRepository;
+            _addressRepository = addrRepository;
+            _employeeRepository = employeeRepository;
+            _actionTypesRepository = actionTypesRepository;
+            _storageRepository = storageRepository;
+
+            _logger = logger;
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfiles(new List<Profile> {
+                new StorageMapperProfile()
+            }));
+
+            _mapper = new Mapper(config);
+        }
+
+
+        [HttpPost("/actions")]
+        public async Task<IActionResult> CreateActionType(
+           string actionName)
+        {
+            StorageActionType action = new StorageActionType(
+                name: actionName,
+                dateCreated: DateTime.Now);
+
+            return Ok(_actionTypesRepository.Create(action));
+        }
+
+
+        [HttpPost("/{storage_id:int}/employees")]
+        public async Task<IActionResult> CreateStorageEmployee(
+           int storage_id,
+           string user_account_id)
+        {
+            StorageEmployee emp = new StorageEmployee(
+                externalUserAccountId: user_account_id,
+                storageId: storage_id,
+                dateCreated: DateTime.Now);
+
+            return Ok(_employeeRepository.Create(emp));
+        }
+
+        [HttpPost("/{storage_id:int}/cells")]
+        public async Task<IActionResult> CreateStorageCell(
+           int storage_id,
+           string title,
+           string comment)
+        {
+            StorageCell cell = new StorageCell(
+                 cellNumber: title,
+                 commentary: comment,
+                 storageId: storage_id);
+
+            var result = _storageCellRepository.Create(cell);
+
+            return Ok(result);
+        }
+
+        [HttpPost("/")]
+        public async Task<IActionResult> CreateStorage(
+            string addrCity,
+            string addrStreet,
+            string addrBuilding)
+        {
+            QueryResult<AddressPoint> addrResult = _addressRepository.Create(new AddressPoint(
+                cityAddr: addrCity,
+                streetAddr: addrStreet,
+                buildingNumberAddr: addrBuilding));
+
+            if (!addrResult.IsSuccessed)
+                return Ok(addrResult);
+
+            MarketStorage store = new MarketStorage(
+                addressId: addrResult.Value.Id);
+
+            QueryResult<MarketStorage> storeResult = _storageRepository.Create(store);
+
+            if (storeResult.IsSuccessed)
+            {
+                _marketplaceProducer.AddMarketplaceStorage(
+                    externalStorageId: storeResult.Value.Id,
+                    city: addrCity,
+                    street: addrStreet,
+                    building: addrBuilding);
+            }
+
+            return Ok(storeResult);
+        }
+
+
+        [HttpGet("/actions")]
+        public async Task<IActionResult> GetAllStorageActions()
+        {
+            var dtos = _actionTypesRepository.GetAll();
+
+            return Ok(_actionTypesRepository.GetAll());
+        }
+
+        [HttpGet("/{storage_id:int}/employees")]
+        public async Task<IActionResult> GetAllStorageEmployees(int storage_id)
+        {
+            var dtos = _employeeRepository.Find(new StorageEmployeesSpec(storage_id));
+
+            return Ok(dtos);
+        }
+
+        [HttpGet("/")]
+        public async Task<IActionResult> GetAllStorages()
+        {
+            var dtos = _storageRepository.GetAll();
+
+            return Ok(_mapper.Map<IEnumerable<MarketStorage>, IEnumerable<StorageRead>>(dtos));
+        }
+
+
+        [HttpGet("/{storage_id:int}/products")]
+        public async Task<IActionResult> GetStorageProducts(int storage_id)
+        {
+            var dtos = _productsRepository.Find(new ProductsOnStorageSpec(
+                onStorageId: storage_id));
+
+            return Ok(dtos);
+        }
+
+        [HttpGet("/{storage_id:int}/cells")]
+        public async Task<IActionResult> GetAllStorageCells(int storage_id)
+        {
+            var dtos = _storageCellRepository.Find(new CellsOnStorageSpec(
+                storageId: storage_id));
+
+            return Ok(dtos);
+        }
+    }
+}
