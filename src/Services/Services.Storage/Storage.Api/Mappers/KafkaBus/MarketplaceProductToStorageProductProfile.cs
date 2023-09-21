49a6ca52b0e1a9192ef9bf155a1179e25fee5c79
ghot <84047808+IndianBleach@users.noreply.github.com>
2023-09-21 13:28:45 +0300

    productStorage: add mappers

---
+++ src/Services/Services.Storage/Storage.Api/Mappers/KafkaBus/MarketplaceProductToStorageProductProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Ozon.Bus.DTOs.StorageService;
+using Storage.Data.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Api.Mappers.KafkaBus
+{
+    public class MarketplaceProductToStorageProductProfile : Profile
+    {
+        public MarketplaceProductToStorageProductProfile()
+        {
+            CreateMap<MarketplaceProductStorageRegistrationRead, StorageProduct>()
+                .ForCtorParam("marketplaceProductId", src => src.MapFrom(x => x.MarketplaceProductId))
+                .ForCtorParam("storageId", src => src.MapFrom(x => x.StorageId))
+                .ForCtorParam("dateAdded", src => src.MapFrom(_ => DateTime.Now));
+        }
+    }
+}
---
+++ src/Services/Services.Storage/Storage.Api/Mappers/StorageMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Common.DTOs.Storage;
+using Storage.Data.Entities.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Infrastructure.Mappers
+{
+    public class StorageMapperProfile : Profile
+    {
+        public StorageMapperProfile()
+        {
+            CreateMap<MarketStorage, StorageRead>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(x => x.Id));
+        }
+    }
+}
