8905fe47e356af9d5493cd6379c5ce061d4711a4
ghot <84047808+IndianBleach@users.noreply.github.com>
2023-09-21 13:21:25 +0300

    marketplace: various project changes

--- src/Services/Services.Marketplace/Marketplace.Api/Program.cs
+++ src/Services/Services.Marketplace/Marketplace.Api/Program.cs
@@ -1,19 +1,38 @@
 using Common.Repositories;
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Marketplace.Api.Kafka.Producers;
 using Marketplace.Data.Context;
+using Marketplace.Infrastructure.BusServices;
 using Marketplace.Infrastructure.Repositories;
 using Marketplace.Infrastructure.Repositories.Products;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
+
+builder.Services.AddHangfire(configuration => configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseMemoryStorage());
+
+builder.Services.AddHangfireServer(options => {
+    options.Queues = new[] { "consumers", "beta", "default" };
+});
 
 var connection = builder.Configuration.GetConnectionString("MssqlConnectionString");
 
-builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection), ServiceLifetime.Transient);
 
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
-builder.Services.AddScoped(typeof(IServiceRepository<>), typeof(ServiceRepository<>));
+builder.Services.AddTransient(typeof(IServiceRepository<>), typeof(ServiceRepository<>));
+
+builder.Services.AddTransient<IMarketplaceProductBusService, MarketplaceProductBusService>();
+
+builder.Services.AddTransient<IProductRegistryProducer, ProductRegistryProducer>();
 
 // elastic, redis
 //builder.Services.AddGrpc();
@@ -25,17 +44,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseHangfireDashboard();
+
+app.UseCors(builder => {
+    builder.AllowAnyOrigin();
+    builder.AllowAnyHeader();
+    builder.AllowAnyMethod();
+});
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
-app.UseCors(x =>
-{
-    x.AllowAnyOrigin();
-    x.AllowAnyHeader();
-});
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
