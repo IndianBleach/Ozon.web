6b845d86490ae7d504706c5bb4ee2e2901b1daa2
ghot <84047808+IndianBleach@users.noreply.github.com>
2023-09-21 13:27:17 +0300

    productRegistry: repository config

---
+++ src/Services/Services.Products/Products.Infrastructure/Specifications/GetProductByIdSpec.cs
@@ -0,0 +1,18 @@
+﻿using Common.Specifications;
+using Products.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Infrastructure.Specifications
+{
+    public class GetProductByIdSpec : BaseSpecification<Product>
+    {
+        public GetProductByIdSpec(string productId) : base(x => x.Id.Equals(productId))
+        {
+        }
+    }
+}
