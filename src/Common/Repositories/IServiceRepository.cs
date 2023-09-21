a36b36893f531126e936fc4891778589ae3c9e96
ghot <84047808+IndianBleach@users.noreply.github.com>
2023-09-21 13:15:45 +0300

    global: update repository interface

--- src/Common/Repositories/IServiceRepository.cs
+++ src/Common/Repositories/IServiceRepository.cs
@@ -11,6 +11,8 @@ namespace Common.Repositories
 {
     public interface IServiceRepository<T> where T : class
     {
+        int AddRange(T[] entities);
+
         QueryResult<T> Create(T entity);
 
         T? FirstOrDefault(ISpecification<T> spec);
